13286308401265e766378e509539f01ca4f9a17b
sbalayan1 <balayans2014@yahoo.com>
2022-11-13 09:48:19 -0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  submitTask()
 });
+
+function submitTask() {
+  const taskList = document.querySelector('#tasks')
+  const taskForm = document.querySelector('#create-task-form')
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const taskDescription = taskForm["new-task-description"]
+    const priority = taskForm["priority"]
+    const li = document.createElement('li')
+    li.textContent = taskDescription.value
+    li.style = `color: ${priority.value}`
+    createDeleteButton(li, taskList)
+    taskList.append(li)
+  })
+}
+
+function createDeleteButton(target, list) {
+  const delButton = document.createElement('button')
+  delButton.textContent = "x"
+  delButton.addEventListener('click', () => {
+    list.removeChild(target)
+  })
+  target.append(delButton)
+}
